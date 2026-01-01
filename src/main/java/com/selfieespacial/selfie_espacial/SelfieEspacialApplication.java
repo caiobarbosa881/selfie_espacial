9cf0214840718707e371c147a25067323e3a766f
Caio Barbosa <54036407+caiobarbosa881@users.noreply.github.com>
2026-01-01 19:08:53 -0300

    inicializando projeto spring boot

---
+++ src/main/java/com/selfieespacial/selfie_espacial/SelfieEspacialApplication.java
@@ -0,0 +1,13 @@
+package com.selfieespacial.selfie_espacial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfieEspacialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfieEspacialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selfieespacial/selfie_espacial/SelfieEspacialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfieespacial.selfie_espacial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfieEspacialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
